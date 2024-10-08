50813178b44f338ae6389b9761875bf408ba1c39
Balla Kavya <ballakavya594@gmail.com>
2024-10-08 10:00:00 +0530

    Added Math Class

---
+++ src/MathDemo/BasicMath.java
@@ -0,0 +1,17 @@
+package MathDemo;
+
+public class BasicMath {
+
+	public static void main(String[] args) {
+		int x=2;
+		int y=16;
+		int z=27;
+		
+		System.out.println("Minimum of Two Numbers : "+Math.min(x, y)); 
+		System.out.println("Maximum of Two Numbers : "+Math.max(x, y)); 
+		System.out.println("SquareRoot of "+y+" : "+Math.sqrt(y));  
+		System.out.println("Cube Root of "+z+" : "+Math.cbrt(y));  
+		System.out.println("Power of two numbers : "+Math.pow(x, y));
+	}
+
+}
